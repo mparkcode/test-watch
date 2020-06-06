4e7079ea947bba12de59821a7e0cc31009fc4c04
Rami <ramifareed1989@hotmail.com>
2020-06-06 15:14:32 +0000

    added the cart and search apps

--- the_watch_agent/urls.py
+++ the_watch_agent/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from accounts.views import index
 from accounts import urls as accounts_urls
 from products import urls as products_urls
+from cart import urls as cart_urls
+from search import urls as search_urls
 from products.views import all_products
 from django.views import static
 from .settings import MEDIA_ROOT
@@ -27,5 +29,7 @@ urlpatterns = [
     url(r'^$', index, name="index"),
     url(r'^accounts/', include(accounts_urls)),
     url(r'^products/', include(products_urls)),
+    url(r'^cart/', include(cart_urls)),
+    url(r'^search/', include(search_urls)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
 ]
