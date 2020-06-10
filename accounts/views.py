ea77a345461571669d087cabf5b281f282174162
Rami <ramifareed1989@hotmail.com>
2020-06-10 13:21:24 +0000

    adding stripe and some modifications

--- accounts/views.py
+++ accounts/views.py
@@ -65,7 +65,3 @@ def registration(request):
         registration_form = UserRegistrationForm()
     return render(request, 'registration.html', {"registration_form": registration_form})
 
-def user_profile(request):
-    """The user's profile page"""
-    user = User.objects.get(email=request.user.email)
-    return render(request, 'profile.html', {"profile": user})
