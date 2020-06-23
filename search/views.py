9aec8bf3877ff7e09a068ab6e4cc225e8fa0f87f
Rami <ramifareed1989@hotmail.com>
2020-06-23 18:21:23 +0000

    added login_required & edited base.html

--- search/views.py
+++ search/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from products.models import Product
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def do_search(request):
     products = Product.objects.filter(name__icontains=request.GET['q'])
     return render(request, "products.html", {"products": products})
