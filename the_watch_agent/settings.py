6e4fc45cbd252baaf5c248bbfa0cee6a87041706
Michael Park <mparkcode@gmail.com>
2020-06-24 02:44:55 +0000

    test

--- the_watch_agent/settings.py
+++ the_watch_agent/settings.py
@@ -115,8 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-    'accounts.backends.CaseInsensitiveAuth'
+    'django.contrib.auth.backends.ModelBackend'
 ]
 
 # Internationalization
